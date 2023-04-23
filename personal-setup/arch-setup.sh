00e4c5550e61a56f768172b8f0eff5a45b8e8159
Rama Bondan Prakoso <ramanarubp@gmail.com>
2023-04-23 20:36:23 +0700

    personal-setup
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- personal-setup/arch-setup.sh
+++ personal-setup/arch-setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
+git config --global user.name "Rama Bondan Prakoso"
+git config --global user.email "ramanarubp@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
 printf '\n' | tee -a ~/.bashrc
-echo "source ~/scripts/functions" >> ~/.bashrc
-echo "onLogin" >> ~/.bashrc
-yaourt -S figlet fortune-mod hub byobu --noconfirm
+echo "source ~/scripts/functions" >> ~/.zshrc
+echo "onLogin" >> ~/.zshrc
+yay -S figlet fortune-mod hub byobu --noconfirm
